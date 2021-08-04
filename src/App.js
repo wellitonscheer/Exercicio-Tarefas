9675fb23b46388fe387b3e0b6cdfb8d5887da5d3
Welliton Scheer <wellitonscheer@gmail.com>
2021-08-04 14:27:24 -0300

    Aprendendo React JS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import {v4 as uuiddv4} from 'uuid'
 import {BrowserRouter as Router, Route} from "react-router-dom"
+import axios from "axios"
 
 import "./app.css"
 import Tasks from "./components/Tasks"
@@ -22,6 +23,16 @@ export default () => {
     }
   ])
 
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const {data} = await axios.get(
+        "https://jsonplaceholder.cypress.io/todos?_limit=10"
+      );
+      setTasks(data)
+    };
+    fetchTasks();
+  },[])
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
       if (task.id === taskId) return { ... task, completed: !task.completed }
